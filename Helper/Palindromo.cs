33c60556fe624794ff00874cd3deb34e28d6edea
Eduardo Vieira <eduardovieira2@gmail.com>
2021-05-10 20:29:32 -0300

    Projeto
    Código fonte da implementaçao da atividade da funcionalidade que valide se uma frase ou palavra é palíndromo.
    escrito em xunit e c#

---
+++ Helper/Palindromo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Helper
+{
+    public class Palindromo
+    {
+        public bool IsPalindromo(string valor)
+        {
+            Regex regex = new Regex("[^a-zA-Z0-9]");
+            var cleaned = regex.Replace(valor.ToLower(), "");
+            var reversed = new string(cleaned.Reverse().ToArray());
+            return (cleaned.Equals(reversed));
+        }
+    }
+
+}
